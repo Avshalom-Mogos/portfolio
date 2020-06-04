695cc3e3e0c7b27963d3c4cc7d751b12dd2dfb69
Avshalom Mogos <avshalomogos@gmail.com>
2020-06-04 22:32:54 +0300

    Updates

---
+++ precache-manifest.ef08e22f9f5be5c26c9a1a3a7ba0e39c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a442d3a8b88dd55f01bd80e5e83e3b7d",
+    "url": "/portpolio/index.html"
+  },
+  {
+    "revision": "af32410a962341f3c7d9",
+    "url": "/portpolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0f3437ca0c120b8e8d0d",
+    "url": "/portpolio/static/css/main.a6ef1633.chunk.css"
+  },
+  {
+    "revision": "af32410a962341f3c7d9",
+    "url": "/portpolio/static/js/2.cdecd02f.chunk.js"
+  },
+  {
+    "revision": "40be5ea31f0ae9d6ed3e05bd4f0b277c",
+    "url": "/portpolio/static/js/2.cdecd02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3437ca0c120b8e8d0d",
+    "url": "/portpolio/static/js/main.7349227f.chunk.js"
+  },
+  {
+    "revision": "2f70ffc2daea42b87aab",
+    "url": "/portpolio/static/js/runtime-main.4228dd87.js"
+  },
+  {
+    "revision": "91776a48d336b5a3f172b918a3304699",
+    "url": "/portpolio/static/media/database.91776a48.svg"
+  },
+  {
+    "revision": "79a6f44f002045a7b7775625b368e705",
+    "url": "/portpolio/static/media/flappyBird.79a6f44f.gif"
+  },
+  {
+    "revision": "d00558195fff9b690ad02a5eab1d1b5b",
+    "url": "/portpolio/static/media/iPodClassic.d0055819.gif"
+  },
+  {
+    "revision": "82855d7b51b49777b5f7618360a6d759",
+    "url": "/portpolio/static/media/jas.82855d7b.gif"
+  },
+  {
+    "revision": "94274716a2b5d7e42383b8ca1b93def5",
+    "url": "/portpolio/static/media/server.94274716.svg"
+  },
+  {
+    "revision": "4b91755ccba86da709126280dee962e9",
+    "url": "/portpolio/static/media/snake.4b91755c.gif"
+  },
+  {
+    "revision": "451c872af0ca7315619d8095afce6155",
+    "url": "/portpolio/static/media/theBookShelf.451c872a.gif"
+  },
+  {
+    "revision": "4833b35cab9c3763460de25880d81e01",
+    "url": "/portpolio/static/media/ticTacToe.4833b35c.gif"
+  },
+  {
+    "revision": "372eaaeda8d3d4c27b00f5c53714a118",
+    "url": "/portpolio/static/media/webpage.372eaaed.svg"
+  }
+]);
